438969539f8812d018b84db397503ae8d543e5d8
devErenNildo <dev.erenildo@gmail.com>
2024-12-02 18:44:15 -0400

    validando municipio existente no put municipio

--- src/main/java/org/enderecosquadra/services/municipio/MunicipioService.java
+++ src/main/java/org/enderecosquadra/services/municipio/MunicipioService.java
@@ -26,31 +26,37 @@ public class MunicipioService {
     @Autowired
     private MunicipioValidacao municipioValidacao;
 
-    // BUSCA TODOS OS MUNICÍPIOS OU APENAS UM DEPENDENDO DOS PARÂMETROS
+    // BUSCA TODOS OS MUNICÍPIOS OU APENAS UM DEPENDENDO DOS PARÂMETROS ==========================================================================
     public List<MunicipioResponseDTO> buscarMunicipioComParametros(Long codigoMunicipio, Long codigoUF, String nome, Integer status) {
         List<Municipio> municipios = municipioRepository.buscarMunicipioPorParametros(codigoMunicipio, codigoUF, nome, status);
         return converterMunicipioEmDTO(municipios);
     }
 
-    // ADICIONAR MUNICÍPIO
+    // ADICIONAR MUNICÍPIO======================================================================================================================
     public List<MunicipioResponseDTO> adicionarMunicipio(MunicipioRequestDTO municipioRequestDTO){
+
+        // VERIFICA SE O NOME DO MUNICIO JA EXISTE NO BANCO DE DADOS
         municipioValidacao.validarNomeMunicipioExistente(municipioRequestDTO.getNome());
 
+        // VERIFICA SE O ID DO ESTADO EXISTE NO BANCO DE DADOS
         UF uf = ufRepository.findById(municipioRequestDTO.getCodigoUF())
                 .orElseThrow(() -> new ExceptionDeRetorno("Uf com o código: " + municipioRequestDTO.getCodigoUF() + " não existe no banco de dados !"));
 
+        // CRIA UM MUNICIPIO USANDO O DTO
         Municipio municipio = new Municipio(
                 uf,
                 municipioRequestDTO.getNome(),
                 municipioRequestDTO.getStatus()
         );
 
+        // SALVA NO BANCO DE DADOS
         municipioRepository.save(municipio);
 
+        // RETORNA A LISTA COM OS MUNICIPIOS
         return converterMunicipioEmDTO(municipioRepository.findAll());
     }
 
-    // EDITAR UM MUNICIPIO
+    // EDITAR UM MUNICIPIO===========================================================================================================================
     public List<MunicipioResponseDTO> editarMunicipio(MunicipioRequestPutDTO municipioRequestPutDTO){
         Municipio municipioExiste = municipioValidacao.verificarSeMunicipioExiste(municipioRequestPutDTO.getCodigoMunicipio());
 
@@ -61,13 +67,16 @@ public class MunicipioService {
         municipioExiste.setNome(municipioRequestPutDTO.getNome());
         municipioExiste.setStatus(municipioRequestPutDTO.getStatus());
 
+        // VERIFICAR SE O NOME DO MUNICIPIO JA EXISTE NO BANCO DE DADOS
+        municipioValidacao.validarNomeMunicipioExistente(municipioExiste.getNome());
+
         municipioRepository.save(municipioExiste);
 
         return buscarMunicipioComParametros(null, null, null, null);
 
     }
 
-    // converter Municipio em municipioDTO para ser exibido
+    // converter Municipio em municipioDTO para ser exibido ==============================================================================================
     private List<MunicipioResponseDTO> converterMunicipioEmDTO(List<Municipio> municipios){
         return municipios.stream()
                 .map(municipio -> new MunicipioResponseDTO(
